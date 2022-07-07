3fbb8b299680267e1faae3d04d3e30398827df9e
Sersoc <45021157+Sersoc@users.noreply.github.com>
2022-07-07 16:16:16 +0900

    connect with db
    connected mysql but have to fix numbering of index

--- NoticeBoard/PostPage.xaml.cs
+++ NoticeBoard/PostPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,19 @@ using System.Windows.Shapes;
 
 namespace NoticeBoard
 {
-    /// <summary>
-    /// PostPage.xaml에 대한 상호 작용 논리
-    /// </summary>
+
     public partial class PostPage : Page
     {
+
+        int index;
+        string connStr = "Server=localhost;Database=test;Uid=root;Pwd=P@ssw0rd!@#$;";
+        string password;
+        bool correction = false;
         public PostPage()
         {
+
             InitializeComponent();
+
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -33,12 +39,57 @@ namespace NoticeBoard
 
         private void DeleteClick(object sender, RoutedEventArgs e)
         {
-
+            if (password == tbxPassword.Text && index != null)
+            {
+                DbConnect(($"DELETE FROM post WHERE bno = {index}"));
+              
+            }
         }
 
         private void CorrectionClick(object sender, RoutedEventArgs e)
         {
+            
+            if (password == tbxPassword.Text && correction == false)
+            {
+                tbxMainText.IsReadOnly = false;
+                correction = true;
 
+            }
+            else if(password == tbxPassword.Text && correction == true)
+            {
+                DbConnect(($"UPDATE  post SET maintext = '{tbxMainText.Text}'"));
+                tbxMainText.IsReadOnly = true;
+                correction = false;
+            }
+        }
+        private void DbConnect(string dbCmd)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            MySqlCommand cmd = new MySqlCommand(dbCmd, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                index = Convert.ToInt32(tbxNo.Text);
+                conn.Open();
+                string sql = $"SELECT * FROM post WHERE bno = {index}";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Title.Text = rdr["title"].ToString();
+                    Writer.Text = rdr["writer"].ToString();
+                    tbxMainText.Text = rdr["maintext"].ToString();
+                    password = rdr["pasw"].ToString();
+                    DateTime.Text = "작성일 " + rdr["writetime"].ToString();
+                }
+                rdr.Close();
+            }
         }
     }
 }
--- NoticeBoard/WritePage.xaml.cs
+++ NoticeBoard/WritePage.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using MySql.Data.MySqlClient;
 namespace NoticeBoard
 {
     /// <summary>
@@ -20,9 +20,11 @@ namespace NoticeBoard
     /// </summary>
     public partial class WritePage : Page
     {
+    
         public WritePage()
         {
             InitializeComponent();
+                  
         }
 
         private void CancleClick(object sender, RoutedEventArgs e)
@@ -30,10 +32,27 @@ namespace NoticeBoard
             Uri uri = new Uri("/MainPage.xaml", UriKind.Relative);
             NavigationService.Navigate(uri);
         }
-
+        
         private void UploadClick(object sender, RoutedEventArgs e)
         {
 
+            string connStr = "Server=localhost;Database=test;Uid=root;Pwd=P@ssw0rd!@#$;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            int index = 0;
+            MySqlCommand ina = new MySqlCommand("SELECT COUNT(*) FROM post", conn);
+            string dbCmd = ($"INSERT INTO post VALUES ('{tbxMainText.Text}', '{tbxPassword.Text}', '{tbxTitle.Text}', '{tbxWriter.Text}','{index}'," +
+                $"'{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}')");
+            MySqlCommand cmd = new MySqlCommand(dbCmd, conn);
+            cmd.ExecuteNonQuery();
+            
+            Uri uri = new Uri("/MainPage.xaml", UriKind.Relative);
+            NavigationService.Navigate(uri);
+
         }
+
+
+
+  
     }
 }
